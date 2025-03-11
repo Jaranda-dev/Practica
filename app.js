a2e1f77bb5b6de43504b350ca04bfd493c4fae56
Angel T <angel.t069@outlook.com>
2025-03-11 17:07:50 -0600

    Add express cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import actorRoutes from './routes/actorRoutes.js'
 import addressRoutes from './routes/addressRoutes.js'
 import categoryRoutes from './routes/categoryRoutes.js'
@@ -18,6 +19,12 @@ import filmTextRoutes from './routes/filmTextRoutes.js'
 
 const app = express()
 const port = 44221
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }));
 
 app.use(express.json())
 app.use('/actores', actorRoutes)
