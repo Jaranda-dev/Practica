7d0389c20662829afd917992632995f873158bdd
Angel T <angel.t069@outlook.com>
2025-03-11 11:18:21 -0600

    Add customers CRUD

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,4 +1,5 @@
 import { prisma } from "../db.js"
+import { validationResult } from "express-validator"
 
 class CustomerController {
     constructor() {
@@ -6,7 +7,7 @@ class CustomerController {
     }
 
     async get(req, res) {
-        const clientes = await prisma.customer.findMany({
+        const customers = await prisma.customer.findMany({
             // include: {
             //     address: true,
             //     store: true,
@@ -14,7 +15,84 @@ class CustomerController {
             //     rental: true
             // }
         })
-        res.json({data: clientes})
+        res.json({data: customers})
+    }
+
+    async show(req, res) {
+        const { id } = req.params
+
+        const customer = await prisma.customer.findFirst({
+            where: {customer_id: Number(id)}
+        })
+
+        res.status(200).json({data: customer})
+    }
+
+    async create(req, res) {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { store_id, first_name, last_name, email, address_id } = req.body
+
+        try {
+            const newCustomer = await prisma.customer.create({
+                data: {
+                    store_id,
+                    first_name,
+                    last_name,
+                    email,
+                    address_id,
+                    create_date: new Date()
+                }
+            })
+            res.status(201).json({data: newCustomer})
+        } catch (error) {
+            console.log(error)
+            res.status(500).send(error)
+        }
+    }
+
+    async update(req, res) {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        
+        const { id } = req.params
+        const { store_id, first_name, last_name, email, address_id } = req.body
+        
+        try {
+            const newCustomer = await prisma.customer.update({
+                where: { customer_id: Number(id) },
+                data: {
+                    store_id,
+                    first_name,
+                    last_name,
+                    email,
+                    address_id
+                }
+            })
+            res.status(200).json({data: newCustomer})
+        } catch (error) {
+            res.status(500).send(error)
+        }
+    }
+
+    async delete(req, res) {
+        const { id } = req.params
+        try {
+            await prisma.customer.delete({
+                where: { customer_id: Number(id)}
+            })
+            res.status(200).json({data: "Cliente eliminado"})
+        } catch (error) {
+            res.status(500).json(error)
+        }
+        
     }
 }
 
--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -1,7 +1,34 @@
 import express from 'express'
 import customerController from '../controllers/customerController.js'
+import { body } from 'express-validator'
 const router = express.Router()
 
 router.get('/', customerController.get)
+
+router.post(
+    '/',
+    [
+        body('store_id').notEmpty().withMessage('Store id is required'),
+        body('first_name').notEmpty().withMessage('First name is required'),
+        body('last_name').notEmpty().withMessage('Last name is required'),
+        body('email').isEmail().notEmpty().withMessage('Last name is required'),
+        body('address_id').notEmpty().withMessage('Address id is required')
+    ],
+    customerController.create
+)
+
+router.route('/:id')
+    .get(customerController.show)
+    .put(
+    [
+        body('store_id').notEmpty().withMessage('Store id is required'),
+        body('first_name').notEmpty().withMessage('First name is required'),
+        body('last_name').notEmpty().withMessage('Last name is required'),
+        body('email').isEmail().notEmpty().withMessage('Last name is required'),
+        body('address_id').notEmpty().withMessage('Address id is required')
+    ],
+    customerController.update
+    )
+    .delete(customerController.delete)
 
 export default router
