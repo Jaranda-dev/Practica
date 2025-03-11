59181a36cc4e491e3f8da6e3b20b5c4d05a29e04
Angel T <angel.t069@outlook.com>
2025-03-11 11:43:03 -0600

    Add films CRUD

--- controllers/filmController.js
+++ controllers/filmController.js
@@ -1,4 +1,5 @@
 import { prisma } from "../db.js"
+import { validationResult } from "express-validator"
 
 class FilmController {
     constructor() {
@@ -16,6 +17,96 @@ class FilmController {
             }
         })
         res.json({data: peliculas})
+    }
+
+    async show(req, res) {
+        const { id } = req.params
+
+        const pelicula = await prisma.film.findFirst({
+            where: {film_id: Number(id)}
+        })
+
+        res.status(200).json({data: pelicula})
+    }
+
+    async create(req, res) {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { title, description, release_year, language_id, original_language_id, rental_duration, rental_rate, length, replacement_cost, rating, special_features } = req.body
+
+        try {
+            const newFilm = await prisma.film.create({
+                data: {
+                    title,
+                    description,
+                    release_year,
+                    language_id,
+                    original_language_id,
+                    rental_duration,
+                    rental_rate,
+                    length,
+                    replacement_cost,
+                    rating,
+                    special_features
+                }
+            })
+            res.status(201).json({data: newFilm})
+        } catch (error) {
+            console.log(error)
+            res.status(500).send(error)
+        }
+    }
+
+    async update(req, res) {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        
+        const { id } = req.params
+        const { title, description, release_year, language_id, original_language_id, rental_duration, rental_rate, length, replacement_cost, rating, special_features } = req.body
+        
+
+        try {
+            const newFilm = await prisma.film.update({
+                where: { film_id: Number(id) },
+                data: {
+                    title,
+                    description,
+                    release_year,
+                    language_id,
+                    original_language_id,
+                    rental_duration,
+                    rental_rate,
+                    length,
+                    replacement_cost,
+                    rating,
+                    special_features
+                }
+            })
+            res.status(200).json({data: newFilm})
+        } catch (error) {
+            console.log(error)
+            res.status(500).send(error)
+        }
+    }
+
+    async delete(req, res) {
+        const { id } = req.params
+        try {
+            await prisma.film.delete({
+                where: { film_id: Number(id)}
+            })
+            res.status(200).json({data: "Pelicula eliminada"})
+        } catch (error) {
+            res.status(500).json(error)
+        }
+        
     }
 }
 
--- routes/filmRoutes.js
+++ routes/filmRoutes.js
@@ -1,7 +1,45 @@
 import express from 'express'
 import filmController from '../controllers/filmController.js'
+import { body } from 'express-validator'
 const router = express.Router()
 
 router.get('/', filmController.get)
 
+router.post(
+    '/',
+    [
+        body('title').notEmpty().withMessage('Title is required'),
+        body('description').optional().isString().withMessage('Description must be a string'),
+        body('release_year').optional().isInt().withMessage('Release year must be an integer'),
+        body('language_id').notEmpty().withMessage('Language id is required'),
+        body('original_language_id').optional().isInt().withMessage('Original language id must be an integer'),
+        body('rental_duration').notEmpty().withMessage('Rental duration is required'),
+        body('rental_rate').notEmpty().withMessage('Rental rate is required'),
+        body('length').optional().isInt().withMessage('Length must be an integer'),
+        body('replacement_cost').notEmpty().withMessage('Replacement cost is required'),
+        body('rating').optional().isString().withMessage('Rating must be a string'),
+        body('special_features').optional().isString().withMessage('Special features must be a string')
+    ],
+    filmController.create
+)
+
+router.route('/:id')
+    .get(filmController.show)
+    .put(
+    [
+        body('title').notEmpty().withMessage('Title is required'),
+        body('description').optional().isString().withMessage('Description must be a string'),
+        body('release_year').optional().isInt().withMessage('Release year must be an integer'),
+        body('language_id').notEmpty().withMessage('Language id is required'),
+        body('original_language_id').optional().isInt().withMessage('Original language id must be an integer'),
+        body('rental_duration').notEmpty().withMessage('Rental duration is required'),
+        body('rental_rate').notEmpty().withMessage('Rental rate is required'),
+        body('length').optional().isInt().withMessage('Length must be an integer'),
+        body('replacement_cost').notEmpty().withMessage('Replacement cost is required'),
+        body('rating').optional().isString().withMessage('Rating must be a string'),
+        body('special_features').optional().isString().withMessage('Special features must be a string')
+    ],
+    filmController.update
+    )
+    .delete(filmController.delete)
 export default router
